3068dbdc93f445c70071af033be1fb0ec8be8e72
Pieter Stevens <122227977+Psteven5@users.noreply.github.com>
2025-03-06 12:02:52 +0100

    Create example_nested.c

---
+++ example_nested.c
@@ -0,0 +1,21 @@
+#define TRY_CATCH_IMPLEMENTATION
+#include "try_catch.h"
+
+void foo(void)
+{
+        TRY
+                THROW(int, 42);
+        CATCH(float, f)
+                printf("%g\n", f);
+        END_TRY
+}
+
+int main(void)
+{
+        TRY
+                foo();
+        CATCH(int, i)
+                printf("%d\n", i);
+        END_TRY
+        return EXIT_FAILURE;
+}
